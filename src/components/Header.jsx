5b6b57340c8a8b6a676e98685a3d0443b0924c60
老师 <neko99tr@gmail.com>
2026-08-31 20:56:46 +0630

    Update website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -105,7 +105,7 @@ const Header = () => {
           "
         >
           <a
-            href="#speciality"
+            href="/contact"
             className="
               w-fit
               rounded-full
